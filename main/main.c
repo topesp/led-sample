5348c4dfaa36311f0fbf47a42746dca7a268a271
sjie <ligongdamao@163.com>
2025-09-11 16:16:04 +0800

    Initial commit: ESP32 LED sample project

---
+++ main/main.c
@@ -0,0 +1,55 @@
+
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+
+#include "iot_button.h"
+#include "button_gpio.h"
+
+#include "driver/gpio.h"
+
+#define TAG "led_sample"
+
+#define SWITCH_GPIO GPIO_NUM_21
+#define LED_GPIO GPIO_NUM_22
+
+static bool switch_state = false;
+
+void handle_switch_button_click(void *arg1, void *arg2) {
+    switch_state = !switch_state;
+    gpio_set_level(LED_GPIO, switch_state);
+    ESP_LOGI(TAG, "BUTTON_SINGLE_CLICK");
+}
+
+void app_main(void)
+{
+    gpio_config_t io_conf = {
+        .pin_bit_mask = (1ULL << LED_GPIO),
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE,
+    };
+    gpio_config(&io_conf);
+
+    // Create button device
+    button_config_t btn_cfg = {
+        .long_press_time = 0,
+        .short_press_time = 0,
+    };
+    button_gpio_config_t btn_gpio_cfg = {
+        .gpio_num = SWITCH_GPIO,
+        .active_level = 0
+    };
+    button_handle_t gpio_btn = NULL;
+    iot_button_new_gpio_device(&btn_cfg, &btn_gpio_cfg, &gpio_btn);
+    if (gpio_btn == NULL) {
+        ESP_LOGE(TAG, "Failed to create button device");
+        abort();
+    }
+    iot_button_register_cb(gpio_btn,BUTTON_SINGLE_CLICK, NULL, &handle_switch_button_click, NULL);
+
+    while (1) {
+        ESP_LOGI(TAG, "LED is %s", switch_state ? "ON" : "OFF");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
